f57c218d376f93dd2930f31c88048b8fb01dc1c5
Mandi Walls <mandi@chef.io>
2018-07-04 14:30:53 +0000

    refactoring hab_deps_search with OptionParser

--- hab_deps_search.rb
+++ hab_deps_search.rb
@@ -4,27 +4,48 @@
 require 'json'
 require 'curb'
 require 'pp'
+require 'optparse'
 
-# read the command line arg
-if ARGV.length < 1
-  puts "Usage: hab_dep_search.rb origin/name/version/release"
-  exit
-else 
-  check_dep = ARGV[0]
-  puts "Checking for #{check_dep}"
+# read the command line args
+options = {}
+
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: hab_deps_search [options]"
+  
+  options[:host] = "localhost"
+  opts.on('-h', '--host HOST', String, 'Hostname or IP to test') do |your_host|
+    options[:host] = your_host
+  end
+  
+  options[:check_dep] = nil
+  opts.on('-d', '--dep DEPENDENCY', String, 'Dependency to check for') do |your_dep|
+    options[:check_dep] = your_dep
+  end
+
+  opts.on('--help', 'Displays help') do
+    puts opts
+    exit
+  end
 end
 
-puts 
+parser.parse!
+begin
+  raise OptionParser::MissingArgument if options[:check_dep].nil?
+rescue OptionParser::MissingArgument
+  puts $!.to_s
+  puts parser
+  exit
+end
+
+
+puts "searching host #{options[:host]} for dep #{options[:check_dep]}" 
 
 # make the request
 # localhost:9631/services
-services_json = Curl.get("http://localhost:9631/services")
-#puts services_json.body_str
+services_json = Curl.get("http://#{options[:host]}:9631/services")
 
 # parse the document
 services_hash = JSON.parse(services_json.body_str)
-#puts services_hash[0]["service_group"]
-#puts services_hash[1]["service_group"]
 
 # We're looking for 
 # .[index][pkg][deps][*].[origin|name|version|release] based on
@@ -33,14 +54,9 @@ services_hash = JSON.parse(services_json.body_str)
 services_hash.each do | service |
   service["pkg"]["deps"].each do | d |
     dep = "#{d['origin']}/#{d['name']}/#{d['version']}/#{d['release']}"
-		if dep.eql? check_dep
+		if dep.eql? options[:check_dep]
 			puts "#{service["pkg"]["ident"]} matches"
 		end
   end
 end
-
-
-# Report as urls?
-# https://bldr.habitat.sh/#/pkgs/$origin/$name/$version/$release
-# probably don't care
 
