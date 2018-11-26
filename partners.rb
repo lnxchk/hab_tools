3d1e5730db09ea2de2a4c0c70d5395d7ce48e85c
mandi walls <mandi.walls@gmail.com>
2018-11-26 20:47:51 +0000

    bug in hash slicing for partners.rb. Quick fix

--- partners.rb
+++ partners.rb
@@ -19,16 +19,16 @@ services_json = Curl.get("http://#{host}:9631/butterfly")
 
 # parse the document
 services_hash = JSON.parse(services_json.body_str)
-#puts services_hash
+#puts services_hash["member"]["members"]
 #puts services_hash.keys
 
 # We're looking for 
 # [member][members][X][address] 
-members_hash = services_hash.slice("member")
+#members_hash = services_hash.slice("member")
 
 puts "My partners are:"
 
-members_hash["member"]["members"].each do | member, v |
+services_hash["member"]["members"].each do | member, v |
   puts v["address"]
 end
 
